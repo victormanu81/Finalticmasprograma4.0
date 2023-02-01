6f133587313fccde2e785fb36902054177be9f25
Victor Sanchez <victormanuster@gmail.com>
2023-02-01 08:01:21 -0300

    Primer Escritura en el repositorio

---
+++ JS/function.js
@@ -0,0 +1,6 @@
+
+document.getElementById('Boton').onclick = function () {
+    console.log("Captura de evento");
+    document.getElementById("Mi pagina").innerHTML = "Gracias por visitar mi CV"
+    
+}
